59ab86d674e297a4ca6864555ff17e675b51351f
syb <syb@gixon.com>
2018-05-22 18:08:51 +0800

    -

--- src/page/article/list.vue
+++ src/page/article/list.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="article-list">
-    
+    <div class="title">作品列表</div>
+    <div class="btn-wrap">
+      <el-input placeholder="输入学员或作品名称识别搜索" v-model="searchName" size="small" style="width:350px;margin-right:10px;">
+        <el-tooltip slot="append" content="搜索" placement="top" >
+          <el-button icon="el-icon-search"></el-button>
+        </el-tooltip>
+      </el-input>
+      <el-tooltip content="刷新" placement="right" >
+        <el-button type="info" icon="el-icon-refresh" plain size="small" ></el-button>
+      </el-tooltip>
+    </div>
+    <div class="list-wrap">
+      
+    </div>
   </div>
 </template>
 
@@ -9,7 +22,7 @@ export default {
   name: 'ArticleList',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      searchName: null
     }
   }
 }
@@ -23,4 +36,15 @@ export default {
     overflow-x: hidden;
   }
   
+  #article-list .title {
+    height: 50px;
+    line-height: 50px;
+    border-bottom: 1px solid #DDDDDD;
+    font-size: 14px;
+  }
+
+  #article-list .btn-wrap {
+    height: 60px;
+    line-height: 60px;
+  }
 </style>
