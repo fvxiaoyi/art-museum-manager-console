22f8f319d0732507c3408289b60d040c86e210da
syb <syb@gixon.com>
2018-06-20 16:24:31 +0800

    -

--- src/page/star/list.vue
+++ src/page/star/list.vue
@@ -20,12 +20,62 @@
         <el-table-column prop="totalStar" label="总赞"></el-table-column>
         <el-table-column fixed="right" label="操作" width="150">
           <template slot-scope="scope">
-            <el-button type="text" size="small">查看详细</el-button>
+            <el-button type="text" size="small" @click="detail(scope.row.id)">查看详细</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
 
+    <el-dialog title="年度点赞统计" :visible.sync="dialogVisible" width="520px">
+      <div>
+        <div class="clear">
+          <div class="left">
+            <el-date-picker value-format="yyyy" v-model="year" type="year" placeholder="选择年" :clearable="false"></el-date-picker>
+            <el-button icon="el-icon-search" @click="detailSearch(detailStar.id)"></el-button>
+          </div>
+          <span class="right">{{detailStar.name}} 总共 <span style="color: #F56C6C;font-weight: bold;">{{detailStar.totalStar}}</span> 个赞</span>
+        </div>
+        <div class="cell">
+          <span>{{year}} 年 <span style="color: #F56C6C;font-weight: bold;">{{detailStar.totalYearStar}}</span> 个赞</span>
+        </div>
+        <div>
+          <table class="detailTable">
+            <tr class="label">
+              <td>一月</td>
+              <td>二月</td>
+              <td>三月</td>
+              <td>四月</td>
+              <td>五月</td>
+              <td>六月</td>
+            </tr>
+            <tr>
+              <td>{{detailStar.januaryStar}}</td>
+              <td>{{detailStar.februaryStar}}</td>
+              <td>{{detailStar.marchStar}}</td>
+              <td>{{detailStar.aprilStar}}</td>
+              <td>{{detailStar.mayStar}}</td>
+              <td>{{detailStar.juneStar}}</td>
+            </tr>
+            <tr class="label">
+              <td>七月</td>
+              <td>八月</td>
+              <td>九月</td>
+              <td>十月</td>
+              <td>十一月</td>
+              <td>十二月</td>
+            </tr>
+            <tr>
+              <td>{{detailStar.julyStar}}</td>
+              <td>{{detailStar.augustStar}}</td>
+              <td>{{detailStar.septemberStar}}</td>
+              <td>{{detailStar.octoberStar}}</td>
+              <td>{{detailStar.novemberStar}}</td>
+              <td>{{detailStar.decemberStar}}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -34,13 +84,33 @@ export default {
   name: 'starList',
   data () {
     return {
+      dialogVisible: true,
       searchParam: {},
-      list: []
+      list: [],
+      detailStar: {},
+      year: new Date().getFullYear() + ''
     }
   },
   methods: {
     search() {
-
+      let me = this
+      this.post('admin/star/list', { name: `%${me.searchParam.name}%`}, (response) => me.list = response.data)
+    },
+    detail(id) {
+      let me = this
+      this.post('admin/star/getForYear', { id, year: me.year }, (response) => {
+        me.dialogVisible = true
+        me.detailStar = response.data
+      })
+    },
+    detailSearch(id) {
+      let me = this
+      if(me.year) {
+        this.post('admin/star/getForYear', { id, year: me.year }, (response) => {
+          me.dialogVisible = true
+          me.detailStar = response.data
+        })
+      }
     }
   }
 }
@@ -69,5 +139,23 @@ export default {
   #star-list .list-wrap {
     margin-top: 12px;
   }
+
+  #star-list .detailTable td {
+    width: 100px;
+    height: 30px;
+    text-align: center;
+    border: 1px solid #DDDDDD;
+  }
   
+  #star-list .label td {
+    font-weight: bold;
+  }
+
+  #star-list .cell {
+    height: 20px;
+    line-height: 20px;
+    font-size: 14px;
+    margin-top: 14px;
+    margin-bottom: 10px;
+  }
 </style>
